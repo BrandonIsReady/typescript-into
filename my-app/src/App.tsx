d1240765a84799ae678cef4c0c6fd4cde0375956
Brandon Trump <brandontrump101@gmail.com>
2022-08-17 19:55:41 +1200

    added brandon t

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <> 
-
-    Brandon
-    <h3>Nisha</h3>
-    
-    
+    <>
+      Brandon
+      <h3>Nisha</h3>
+      <h3>Brandon T</h3>
     </>
   );
 }
